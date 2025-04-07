f2dc4c300fab79248975635e1a62ee34b9f7f10b
Eniiwinner <abrahameniola624@gmail.com>
2025-04-07 02:14:45 -0500

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const mongoURI = 'mongodb+srv://eniiwinner:<8KE076mhgYc0t8cS>@cluster0.ds4fi1q.mongodb.net/devops?retryWrites=true&w=majority'
+const mongoURI = `mongodb+srv://eniiwinner:${process.env.MONGO_PASSWORD}@cluster0.ds4fi1q.mongodb.net/devops?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost/devops', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const Dev = mongoose.model('Dev', { name: String });
 
 app.get('/', async (req, res) => {
-  const devs = await Dev.find();
-  const names = devs.map(d => `<li>${d.name}</li>`).join('');
-  res.send(`<h1>I’m building pipelines like a pro!</h1><ul>${names}</ul>`);
-});
-
-app.listen(3000, () => console.log('App running on port 3000'));
-
-
-
+  try {
+    const devs = await Dev.find();
+    const names = devs.map(d => `<li>${d.name}</li>`).join('');
+    res.send(`<h1>I’m building pipelines like a pro!</h1><ul>${names}</ul>`);
+  } catch (err) {
+    res.status(500).send('Error retrieving developers
